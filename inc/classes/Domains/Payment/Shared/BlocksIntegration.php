b3a4f9cbe88433fc417bd688d0eb270f99309bd8
J7 <j7.dev.gg@gmail.com>
2025-08-12 12:52:24 +0800

    chore: add return type

--- inc/classes/Domains/Payment/Shared/BlocksIntegration.php
+++ inc/classes/Domains/Payment/Shared/BlocksIntegration.php
@@ -24,12 +24,8 @@ class BlocksIntegration extends \Automattic\WooCommerce\Blocks\Payments\Integrat
 	/** @var string 付款方式 id， 只是 AbstractPaymentMethodType 把它定義為 name */
 	protected $name;
 
-	/** @var AbstractPaymentGateway 付款方式 */
-	// private AbstractPaymentGateway $gateway;
-
 	/** @param AbstractPaymentGateway $gateway 付款方式 */
-	public function __construct( private AbstractPaymentGateway $gateway ) {
-		$this->gateway = $gateway;
+	public function __construct( private readonly AbstractPaymentGateway $gateway ) {
 		$this->name    = $gateway->id;
 	}
 
@@ -37,25 +33,20 @@ class BlocksIntegration extends \Automattic\WooCommerce\Blocks\Payments\Integrat
 	 * @return void 初始化
 	 * @throws \Exception 如果未設定 $gateway 或 name 與付款方式 id 不同
 	 * */
-	public function initialize() {
-		if (!isset($this->gateway)) {
-			throw new \Exception('AbstractBlocksIntegration 必須設定 $gateway 屬性');
-		}
-
+	public function initialize(): void {
 		if ($this->name !== $this->gateway->id) {
 			throw new \Exception('AbstractBlocksIntegration 的 name 必須與付款方式 id 相同');
 		}
-
 		$this->settings = \get_option("woocommerce_{$this->name}_settings", []);
 	}
 
 	/** @return boolean 是否啟用 */
-	public function is_active() {
+	public function is_active(): bool {
 		return $this->gateway->is_available();
 	}
 
 	/** @return array<string> 區塊結帳支援的腳本 */
-	public function get_payment_method_script_handles() {
+	public function get_payment_method_script_handles(): array {
 		$handle = "wc-{$this->name}-blocks-integration";
 		\wp_register_script(
 					$handle,
@@ -78,20 +69,9 @@ class BlocksIntegration extends \Automattic\WooCommerce\Blocks\Payments\Integrat
 
 		return [ $handle ];
 	}
-
-	/**
-	 * Returns an array of script handles to be enqueued for the admin.
-	 *
-	 * Include this if your payment method has a script you _only_ want to load in the editor context for the checkout block.
-	 * Include here any script from `get_payment_method_script_handles` that is also needed in the admin.
-	 */
-	public function get_payment_method_script_handles_for_admin() {
-		return $this->get_payment_method_script_handles();
-	}
-
-
-	/** @return array<string, mixed> 給前端取得的付款方式資料 */
-	public function get_payment_method_data() {
+    
+    /** @return array<string, mixed> 給前端取得的付款方式資料 */
+	public function get_payment_method_data(): array {
 		return [
 			'name'              => $this->name,
 			'title'             => $this->gateway->payment_label,
